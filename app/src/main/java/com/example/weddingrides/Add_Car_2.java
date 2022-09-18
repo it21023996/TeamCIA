9f3b29c4eda8fa6f1b91541472360bad9abee048
Isuru Herath <it21041648@my.sliit.lk>
2022-09-18 14:55:50 +0530

    progress 3

--- app/src/main/java/com/example/weddingrides/Add_Car_2.java
+++ app/src/main/java/com/example/weddingrides/Add_Car_2.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Add_Car_2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_vehicles);
+        setContentView(R.layout.activity_add_car2);
     }
 }
--- app/src/main/java/com/example/weddingrides/Edit_Car.java
+++ app/src/main/java/com/example/weddingrides/Edit_Car.java
@@ -1,14 +1,13 @@
 package com.example.weddingrides;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Edit_Car extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_vehicles);
+        setContentView(R.layout.activity_edit_car);
     }
 }
